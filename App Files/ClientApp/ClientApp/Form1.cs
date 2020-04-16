c09cebd1d6057eb1d2cb4da7513ea840992cbf40
boucetta <rachid.bouctta1997@gmail.com>
2020-04-16 22:48:58 +0100

    install app whait dataBade

---
+++ App Files/ClientApp/ClientApp/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string q = "select * from Product";
+            Program.FillDataGridView(dataGridView1, q);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string q1 = "insert into Product values('" + textBox1.Text + "','" + textBox2.Text + "')";
+            string q2 = "select * from Product";
+            Program.Excute(q1);
+            Program.FillDataGridView(dataGridView1,q2);
+        }
+    }
+}
---
+++ App Files/ClientApp/ClientApp/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientApp
+{
+    static class Program
+    {
+        static string path = Path.GetFullPath(Environment.CurrentDirectory);
+        static string databaseName = "Client.mdf";
+        public static SqlConnection cnx = new SqlConnection(@"Data Source=(localdb)\v11.0;AttachDbFilename="+path+@"\"+databaseName+";Integrated Security=True");
+        public static SqlCommand cmd;
+        public static SqlDataReader dr;
+        public static DataTable dt;
+
+        public static void OpenCnx()
+        {
+            if (cnx.State != ConnectionState.Open)
+                cnx.Open();
+        }
+        public static void CloseCnx()
+        {
+            if (cnx.State != ConnectionState.Closed)
+                cnx.Close();
+        }
+        public static void Excute(string req)
+        {
+            cmd = new SqlCommand(req, cnx);
+            OpenCnx();
+            cmd.ExecuteNonQuery();
+            CloseCnx();
+        }
+        public static SqlDataReader FillDataReader(string req)
+        {
+            cmd = new SqlCommand(req, cnx);
+            OpenCnx();
+            dr = cmd.ExecuteReader();
+            return dr;
+        }
+        public static void FillTable(string req)
+        {
+            OpenCnx();
+            cmd = new SqlCommand(req, cnx);
+            dr = cmd.ExecuteReader();
+            dt = new DataTable();
+            dt.Load(dr);
+            CloseCnx();
+        }
+        public static void FillDataGridView(DataGridView d,string req)
+        {
+            FillTable(req);
+            d.DataSource = dt;
+        }
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
